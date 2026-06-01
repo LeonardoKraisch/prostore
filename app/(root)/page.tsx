593f4e371bda39bdd7c63d73e62f46f3ebb04d70
Leonardokraisch <leonardokraisch@outlook.com>
2026-06-01 14:29:57 -0300

    seed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,11 +4,7 @@ import { getLatestProducts } from "@/lib/actions/product.actions";
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
 
-  return (
-    <>
-      <ProductList data={latestProducts} title="Newest Arrivals" />
-    </>
-  );
+  return <ProductList data={latestProducts} title="Newest Arrivals" />;
 };
 
 export default Homepage;
