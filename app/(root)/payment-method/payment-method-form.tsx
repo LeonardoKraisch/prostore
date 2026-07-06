bb63c49f3053956b4e2ee27c2412521d8db4694e
leonardokraisch <leonardokraisch@outlook.com>
2026-07-06 15:59:16 -0300

    feat: implement payment method selection page with radio group component

--- app/(root)/payment-method/payment-method-form.tsx
+++ app/(root)/payment-method/payment-method-form.tsx
@@ -1,15 +1,25 @@
 "use client";
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useToast } from "@/hooks/use-toast";
 import { useTransition } from "react";
 import { paymentMethodSchema } from "@/lib/validators";
 import { updateUserPaymentMethod } from "@/lib/actions/user.actions";
-import CheckoutSteps from "@/components/shared/checkout-steps";
 import { useForm } from "react-hook-form";
 import z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { DEFAULT_PAYMENT_METHOD } from "@/lib/constants";
+import { DEFAULT_PAYMENT_METHOD, PAYMENT_METHODS } from "@/lib/constants";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader } from "lucide-react";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const PaymentMethodForm = ({
   preferredPaymentMethod,
@@ -27,10 +37,86 @@ const PaymentMethodForm = ({
     },
   });
 
+  const onSubmit = async (values: z.infer<typeof paymentMethodSchema>) => {
+    startTransition(async () => {
+      const res = await updateUserPaymentMethod(values);
+
+      if (!res.success) {
+        toast({
+          variant: "destructive",
+          description: res.message,
+        });
+      } else {
+        toast({
+          variant: "default",
+          description: res.message,
+        });
+        router.push("/place-order");
+      }
+    });
+  };
+
   return (
     <div>
-      <CheckoutSteps current={2} />
-      <h1>Payment Method</h1>
+      <div className="max-w-md mx-auto space-y-4">
+        <h1 className="h2-bold mt-4">Payment Method</h1>
+        <p className="text-sm text-muted-foreground">
+          Please select a payment method
+        </p>
+        <Form {...form}>
+          <form
+            method="post"
+            className="space-y-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <div className="flex flex-col md:flex-row gap-5">
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => {
+                  return (
+                    <FormItem className="space-y-3">
+                      <FormControl>
+                        <RadioGroup
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                          className="flex flex-col"
+                        >
+                          {PAYMENT_METHODS.map((method) => (
+                            <FormItem
+                              key={method}
+                              className="flex items-center space-x-3 space-y-0"
+                            >
+                              <FormControl>
+                                <RadioGroupItem value={method} />
+                              </FormControl>
+                              <FormLabel className="font-normal">
+                                {method}
+                              </FormLabel>
+                            </FormItem>
+                          ))}
+                        </RadioGroup>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  );
+                }}
+              />
+            </div>
+
+            <div className="flex gap-2">
+              <Button type="submit" disabled={isPending}>
+                {isPending ? (
+                  <Loader className="animate-spin h-4 w-4" />
+                ) : (
+                  <ArrowRight className="h-4 w-4" />
+                )}
+                Continue
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 };
